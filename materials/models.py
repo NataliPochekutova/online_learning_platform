56e42f4700e50118cfcce59fab3a381263534450
Natalia Pochekutova <natalipochekutova51@gmail.com>
2025-09-07 21:25:26 +0800

    homework_32_1_v.1

--- materials/models.py
+++ materials/models.py
@@ -74,3 +74,26 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Пользователь"
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Курс"
+    )
+
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+    def __str__(self):
+        return f"{self.user} - {self.course}"
--- materials/serializers.py
+++ materials/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import validate_third_party_resources
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    url_video = serializers.URLField(required=False, validators=[validate_third_party_resources])
+
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
 
     lessons = LessonSerializer(many=True, read_only=True)
     count_lessons_in_course = SerializerMethodField()
 
     def get_count_lessons_in_course(self, course):
         return Lesson.objects.filter(course=course).count()
+
+    subscription_to_course = serializers.SerializerMethodField()
+
+    def get_subscription_to_course(self, course):
+        return Subscription.objects.filter(course=course).exists()
 
     class Meta:
         model = Course
